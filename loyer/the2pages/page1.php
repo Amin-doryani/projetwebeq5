b5aeaf89e1c25fabe62147db918c5cf3d0cd7564
Mohamed Amin ED-DARYANI <89279735+Amin-doryani@users.noreply.github.com>
2024-04-19 15:52:24 +0200

    Add files via upload

--- loyer/the2pages/page1.php
+++ loyer/the2pages/page1.php
@@ -37,6 +37,34 @@ main{
      width: 60%;
      
 }
+.divofmain3{
+    display:none;
+    position: fixed;
+    top:0;
+    right: 0;
+    height: 100vh;
+    width:100%;
+    flex-direction: column;
+    justify-content: flex-start;
+    background-color:white;
+    row-gap:20px;
+
+}
+.divofmain3 a{
+    text-decoration: none;
+    color: black;
+    
+    padding-left:10px;
+    width:100%;
+    font-size:2em;
+}
+.themenuicon{
+    width:50px;
+    display:none;
+}
+.divofmain3 a:hover{
+    background-color: #f0f0f0;
+}
 .divofmain2 a{
     text-decoration: none;
     font-size: 1.3em;
@@ -122,7 +150,7 @@ header{
     height: 70px;
 
 }
-.resdiv form  a{
+.resdiv form  .oldainform{
     width: 40px;
     height: 40px;
     background-image: url(./images/resimage.png);
@@ -130,16 +158,126 @@ header{
     border-radius: 2px;
 
 }
+.newainform{
+    display:none;
+}
+.thepofthemenu{
+    display:none;
+}
+@media (max-width:1350px) {
+    .hederbtn{
+        width:50%;
+    }
+}
+@media (max-width:827px) {
+    .hederbtn{
+        width:70%;
+    }
+    .thepofthemenu{
+        display:block;
+    }
+    .divofmain3{
+        display:none;
+    }
+    .divofmain2{
+        display:none;
+    }
+    .thecloseicon{
+        display:block;
+        width:45px;
+        height: 45px;
+        
+    }
+    .themenuicon{
+        display:block;
+    }
+    
+
+
+}
+@media (max-width:800px){
+    .resdiv{
+        height: max-content;
+
+
+    }
+    .resdiv form{
+        flex-direction: column;
+        row-gap:20px;
+        padding:50px;
+    }
+    .oldainform{
+        display:none;
+    }
+    .newainform{
+        display:block;
+        width: max-content !important;
+        background-image: none !important;
+        color:black;
+        text-decoration: none;
+        background-color:white !important;
+        text-align: center;
+        height: max-content !important;
+        padding: 10px 15px !important;
+        
+    }
+    .hederh1{
+        width:80%;
+    }
+}
+    @media (max-width:600px){
+    .hederbtn{
+        
+        display:flex;
+        flex-direction: column !important;
+        justify-content: center;
+        align-items: center;
+        row-gap:30px;
+        
+        
+        
+    }
+    .header{
+        display:flex;
+        flex-direction: column !important;
+        justify-content: center;
+        align-items: center;
+        border:0px solid black !important;
+
+    }
+    .hederh1{
+        width:80%;
+    }
+
+
+}
+    
+
 </style>
 
 <main>
-        <div class="divofmain1">
+        <div class="divofmain1" href="index.php">
             <img src="./images/logo.jfif" alt="logo" class="logoimage">
 
             <h1 class="logoname">LOYER.ma</h1>
         </div>
+        <!-- <img class="themenuicon"  onclick=showmenu() src="./images/menu.png" alt="img"> -->
+        <p onclick=showmenu() class="thepofthemenu">
+        <svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 -960 960 960" width="50"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+        </p>
         <div class="divofmain2">
-            <a href="#">HOME</a>
+
+            <a href="index.php">HOME</a>
+            <a href="#">ABOUT US</a>
+            <a href="#">CONTACT</a>
+            <a href="#">POLICY</a>
+        </div><div class="divofmain3">
+        <!-- <img class="thecloseicon" onclick=closemenu() src="./images/close1.png" alt="img"> -->
+        <p onclick=closemenu()>
+        <svg xmlns="http://www.w3.org/2000/svg" height="50"  viewBox="0 -960 960 960" width="50"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+        </p>
+
+            <a href="index.php">HOME</a>
             <a href="#">ABOUT US</a>
             <a href="#">CONTACT</a>
             <a href="#">POLICY</a>
@@ -159,9 +297,20 @@ header{
             <input type="text" name="ville" id="ville" placeholder="ville" required>
             <input type="number" name="minval" id="minval" placeholder="min prix" required>
             <input type="number" name="maxval" id="maxval"  placeholder="max prix" required>
-            <a href="#" onclick="document.getElementById('form_id').submit();"></a>
+            <a href="#" class='newainform' onclick="document.getElementById('form_id').submit();">Rechercher</a>
+            <a href="#" class='oldainform' onclick="document.getElementById('form_id').submit();"></a>
             
 
         </form>
 </div>
+<script>
+    function closemenu(){
+        const pg = document.querySelector('.divofmain3');
+        pg.style.display = "none";
+    }
+    function showmenu(){
+        const pg = document.querySelector('.divofmain3');
+        pg.style.display = "flex";
+    }
+</script>
     
--- loyer/the2pages/page2.php
+++ loyer/the2pages/page2.php
@@ -14,7 +14,7 @@
             <h1 class="logoname">LOYER.ma</h1>
         </div>
         <div class="divofmain2">
-            <a href="#" class="maina">HOME</a>
+            <a href="index.php" class="maina">HOME</a>
             <a href="#" class="maina">ABOUT US</a>
             <a href="#" class="maina">CONTACT</a>
             <a href="#" class="maina">POLICY</a>
@@ -22,6 +22,7 @@
             <img src="<?php echo $theimgurlp; ?>" alt="image" class="profileimg" onclick="profilem()" >
         </div>
     </main>
+    
     <?php 
     require_once('./conndb.php');
    
@@ -34,6 +35,39 @@
     $prenomC = $row3["prenom"];
 
     ?>
+    <main class="main22" href="index.php">
+        <div class="divofmain1">
+            <img src="./images/logo.jfif" alt="logo" class="logoimage">
+
+            <h1 class="logoname">LOYER.ma</h1>
+        </div>
+        
+        <p onclick=showmenu()>
+        <svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 -960 960 960" width="50"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+        </p>
+        <div class="main22divofmain2">
+        <p onclick=closemenu()>
+        <svg xmlns="http://www.w3.org/2000/svg" height="50"  viewBox="0 -960 960 960" width="50"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+        </p>
+        <p class="pofthemainofachraf"><img src="<?php echo $theimgurlp; ?>" alt="image" class="profileimg"  >  <span><?php echo $nameC." ".$prenomC?></span></p>
+
+            <a href="index.php" class="maina">HOME</a>
+            <a href="#" class="maina">ABOUT US</a>
+            <a href="#" class="maina">CONTACT</a>
+            <a href="#" class="maina">POLICY</a>
+            <a href="publi.php" id="publierbtn2">Publier une annonce</a>
+            <a href="profile.php">Profil</a>
+            <a href="pofileimg.php">Modifier l’image</a>
+            <a href="gestiondpub.php">Publications</a>
+            <a href="saved.php">Annonces enregistrées</a>
+
+            <a href="messagespage.php">Messages</a>
+            <a href="index.php" onclick="desco()" >Deconnexion (  <?php echo $nameC?>)</a>
+
+            
+        </div>
+        
+    </main>
     <div id="ourprofilediv">
         
         <a href="profile.php">Profil</a>
@@ -41,8 +75,8 @@
         <a href="gestiondpub.php">Publications</a>
         <a href="saved.php">Annonces enregistrées</a>
 
-        <a href="#">Messages</a>
-        <a href="main.php" onclick="desco()" >Deconnexion (  <?php echo $nameC?>)</a>
+        <a href="messagespage.php">Messages</a>
+        <a href="index.php" onclick="desco()" >Deconnexion (  <?php echo $nameC?>)</a>
         
     </div>
     <header>
@@ -56,10 +90,11 @@
     </header>
     <div class="resdiv">
         <form action="search.php" method = 'Post' id="form_id">
-            <input type="text" name="ville" id="ville" placeholder="ville" required>
-            <input type="number" name="minval" id="minval" placeholder="min prix" required>
-            <input type="number" name="maxval" id="maxval"  placeholder="max prix" required>
-            <a href="#" onclick="document.getElementById('form_id').submit();"></a>
+            <input type="text" name="ville" id="ville" placeholder="ville" required pattern="^[^<>]*$|.*(\s|>|<)[^<>]*">
+            <input type="number" name="minval" id="minval" placeholder="min prix" required pattern="^[^<>]*$|.*(\s|>|<)[^<>]*">
+            <input type="number" name="maxval" id="maxval"  placeholder="max prix" required pattern="^[^<>]*$|.*(\s|>|<)[^<>]*">
+            <a href="#" class='newainform' onclick="document.getElementById('form_id').submit();">Rechercher</a>
+            <a href="#" class='oldainform' onclick="document.getElementById('form_id').submit();"></a>
             
 
         </form>
@@ -78,6 +113,15 @@
         };
         
         
+    function closemenu(){
+        const pg = document.querySelector('.main22divofmain2');
+        pg.style.display = "none";
+    }
+    function showmenu(){
+        const pg = document.querySelector('.main22divofmain2');
+        pg.style.display = "flex";
+    }
+
 
     </script>
 </body>
